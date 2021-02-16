52a812aca84a1da89be897f90b2d618f1144c763
svdunaev <54572104+svdunaev@users.noreply.github.com>
2021-02-16 07:05:54 +0500

    добавляет компоненты login, favs, 404, offer, делает маршрутизацию композицией

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
 import HomeScreen from "../home-screen/home-screen";
+import LoginScreen from "../login-screen/login-screen";
+import FavScreen from "../fav-screen/fav-screen";
+import OfferScreen from "../offer-screen/offer-screen";
+import PageNotFound from "../404/404";
 import {CardType} from "../../common-prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 const App = (props) => {
   const {cards} = props;
   return (
-    <HomeScreen cards={cards} />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <HomeScreen cards={cards} />
+        </Route>
+        <Route path="/login" exact>
+          <LoginScreen />
+        </Route>
+        <Route path="/favorites" exact>
+          <FavScreen />
+        </Route>
+        <Route path="/offer/:id">
+          <OfferScreen />
+        </Route>
+        <Route>
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
